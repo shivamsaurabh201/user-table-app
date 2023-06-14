28e77b9313f6253789ed9ffdb89e8ebc2a1ed9c0
Shivam Saurabh <shivamsaurabh201@gmail.com>
2023-06-15 00:11:10 +0530

    fetch

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserTable from './UserTable';
+
+const App = () => {
+  return (
+    <div>
+      <h1>User Table</h1>
+      <UserTable />
+    </div>
+  );
+};
+
+export default App;
